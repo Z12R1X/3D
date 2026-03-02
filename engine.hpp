b6b1f1b9ab6175c960c32b1b55d6b1b0c92a8b81
Z12R1X <yakov_kutovenko@mail.ru>
2026-03-02 19:33:00 +0700

    made code clean

--- engine.hpp
+++ engine.hpp
@@ -1,51 +1,32 @@
 #pragma once
-
 #include <SDL2/SDL.h>
 #include <vector>
 #include <cmath>
+#include <numbers>
 #include <chrono>
 
-struct vec3d
-{
-  float x, y, z, w;
-  vec3d() : x(0), y(0), z(0), w(1) {};
-  vec3d(float xCor, float yCor, float zCor) : x(xCor), y(yCor), z(zCor), w(1) {};
+struct vec3d{
+	float x,y,z,w;
+  	vec3d():x(0),y(0),z(0),w(1){};
+  	vec3d(float xCor, float yCor, float zCor):x(xCor),y(yCor),z(zCor),w(1){};
 };
-
-struct triangle
-{
-  vec3d p[3];
+struct triangle{
+  	vec3d p[3];
 };
+using mat4x4=float[4][4];
+using mesh=std::vector<triangle>;
 
-/*struct mesh
-{
-  std::vector<triangle> tris;
-};*/
-
-/*struct mat4x4
-{
-  float m[4][4] = {0};
-};*/
-
-using mat4x4 = float[4][4];
-//using triangle = vec3d[3];
-using mesh = std::vector<triangle>;
-
-class Engine3D
-{
-public:
-  Engine3D(SDL_Window *window, SDL_Renderer *render, mesh &obj);
-  void Render(float speedOfRotation);
-  //void RenderRotate();
-  void Create();
-
-private:
-  SDL_Renderer *renderer;
-  mesh meshCube;
-  mat4x4 matProj;
-  float fTheta;
-  float deltaTime=0.0f;
-  int ScreenWidth, ScreenHeight;
-  // std::vector<triangle> figura;
-  void MultiplyMatrixVector(vec3d &i, vec3d &o, mat4x4 &m);
+class Engine3D{
+	public:
+		Engine3D(SDL_Window* window, SDL_Renderer* render, mesh& obj);
+		void Render(const float speedOfRotation);
+		void Create();
+	private:
+		SDL_Renderer* renderer;
+		mesh meshCube;
+		mat4x4 matProj={0};
+		float fTheta;
+		float deltaTime=0.0f;
+		int ScreenWidth,ScreenHeight;
+		void MultiplyMatrixVector(const vec3d& i, vec3d& o,const mat4x4& m);
 };
--- main.cpp
+++ main.cpp
@@ -1,6 +1,3 @@
-#include <SDL2/SDL.h>
-// #include "render3d.hpp"
-// #include "vector3D.hpp"
 #include "engine.hpp"
 
 // cube
@@ -45,85 +42,35 @@ mesh halfPira = {tri19, tri20, tri21, tri22};
 
 triangle tri23 = {vec3d(1.0f, 1.0f, 1.0f), vec3d(2.0f, 1.0f, 1.0f), vec3d(2.0f, 2.0f, 2.0f)};
 mesh trian{tri23};
-/*std::vector<Point3d> points{
-    Point3d{-1.0f, -1.0f, -1.0f},
-    Point3d{-1.0f, -1.0f, 1.0f},
-    Point3d{1.0f, -1.0f, -1.0f},
-    Point3d{-1.0f, 1.0f, -1.0f},
-    Point3d{-1.0f, 1.0f, 1.0f},
-    Point3d{1.0f, -1.0f, 1.0f},
-    Point3d{1.0f, 1.0f, -1.0f},
-    Point3d{1.0f, 1.0f, 1.0f},
-};
-
-std::vector<Vertex> vertices{
-    Vertex{0, 1},
-    Vertex{0, 2},
-    Vertex{0, 3},
-    Vertex{2, 5},
-    Vertex{3, 6},
-    Vertex{3, 4},
-    Vertex{4, 7},
-    Vertex{6, 7},
-    Vertex{7, 5},
-    Vertex{5, 1},
-    Vertex{4, 1},
-    Vertex{2, 6},
-};*/
 
 int main()
 {
-  /*SDL_Window *window;
-  SDL_Renderer *renderer;
-  window = SDL_CreateWindow("3D Renderer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 960, 540, 0);
-  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-  bool running = true;
-  Render3d render1(window, renderer, points, vertices);
-  while (running)
-  {
-    if (SDL_QuitRequested())
-    {
-      running = false;
-      break;
-    }
-    render1.Render();
-    // render1.RenderRotate();
-  }
-  return 0;*/
   SDL_Window *window;
   SDL_Renderer *renderer;
   window = SDL_CreateWindow("3D Renderer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 960, 540, 0);
   renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
   bool running = true;
   bool rend = true;
-  Engine3D render(window, renderer, trian);
+  Engine3D render(window, renderer, halfPira);
   SDL_Event event;
-  while (running)
-  {
-    if (SDL_PollEvent(&event))
-    {
-      switch (event.type)
-      {
-      case SDL_KEYDOWN:
-        rend = false;
-        break;
-      case SDL_KEYUP:
-        rend = true;
-        break;
-      default:
-        break;
+  while (running){
+    if (SDL_PollEvent(&event)){
+      switch (event.type){
+		case SDL_KEYDOWN:
+			rend = false;
+			break;
+		case SDL_KEYUP:
+			rend = true;
+			break;
+		default:
+			break;
       }
-    }
-    if (SDL_QuitRequested())
-    {
+    }if (SDL_QuitRequested()){
       running = false;
       break;
+    }if (rend){
+      render.Render(1.0f);
     }
-    if (rend)
-    {
-      render.Render(3.0f);
-    }
-    // render.Render();
   }
   return 0;
 }
