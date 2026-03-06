999df564a8a05e1eb14ef2f54702ad7f7e12c9b3
Z12R1X <yakov_kutovenko@mail.ru>
2026-03-07 00:49:39 +0700

    clear a little bit

--- engine.cpp
+++ engine.cpp
@@ -4,11 +4,64 @@ float vec3d::dotProduct(const vec3d& vec1,const vec3d& vec2){
 	return vec1.x*vec2.x+vec1.y*vec2.y+vec1.z*vec2.z;
 }
 
+vec3d vec3d::crossProduct(const vec3d& vec1,const vec3d& vec2){
+	vec3d res;
+	res.x=vec1.y*vec2.z-vec1.z*vec2.y;
+	res.y=vec1.z*vec2.x-vec1.x*vec2.z;
+	res.z=vec1.x*vec2.y-vec1.y*vec2.x;
+	return res;
+}
+
+vec3d vec3d::normal(const vec3d& vec1,const vec3d& vec2){
+	vec3d res=crossProduct(vec1,vec2);
+	res/=res.len();
+	return res;
+}
+
+float vec3d::len(){
+	return sqrt(this->x*this->x+this->y*this->y+this->z*this->z);
+}
+
+vec3d vec3d::operator+(const vec3d& vec){
+	vec3d res={this->x+vec.x,this->y+vec.y,this->z+vec.z};
+	return res;
+}
+
+vec3d vec3d::operator-(const vec3d& vec){
+	vec3d res={this->x-vec.x,this->y-vec.y,this->z-vec.z};
+	return res;
+}
+
+vec3d vec3d::operator*(float scalar){
+	vec3d res={this->x*scalar,this->y*scalar,this->z*scalar};
+	return res;
+}
+
+vec3d vec3d::operator/(float scalar){
+	vec3d res={this->x/scalar,this->y/scalar,this->z/scalar};
+	return res;
+}
+
+void vec3d::operator+=(const vec3d& vec){
+	this->x+=vec.x;this->y+=vec.y;this->z+=vec.z;
+}
+
+void vec3d::operator-=(const vec3d& vec){
+	this->x-=vec.x;this->y-=vec.y;this->z-=vec.z;
+}
+
+void vec3d::operator*=(float scalar){
+	this->x*=scalar;this->y*=scalar;this->z*=scalar;
+}
+
+void vec3d::operator/=(float scalar){
+	this->x/=scalar;this->y/=scalar;this->z/=scalar;
+}
+
 Engine3D::Engine3D(SDL_Window *window_, SDL_Renderer* renderer_){
 	SDL_GetWindowSize(window_, &ScreenWidth, &ScreenHeight);
 	renderer=renderer_;
-	//meshCube=obj;
-	Create();
+	CreateProjMatrix();
 }
 
 void Engine3D::MultiplyMatrixVector(const vec3d &i, vec3d &o,const mat4x4 &m){
@@ -46,11 +99,10 @@ bool Engine3D::loadMesh(const std::string& filename){
 			s>>junk>>f[0]>>f[1]>>f[2];
 			meshObject.push_back({vertsCache[f[0]-1],vertsCache[f[1]-1],vertsCache[f[2]-1]});
 		}
-	}
-	return true;
+	}return true;
 }
 
-void Engine3D::Create(){
+void Engine3D::CreateProjMatrix(){
 	// Projection Matrix
 	float fNear=0.1f;
 	float fFar=1000.0f;
@@ -71,63 +123,51 @@ void Engine3D::Render(const float speedOfRotation)
 	SDL_SetRenderDrawColor(renderer,0,0,0,SDL_ALPHA_OPAQUE);
 	SDL_RenderClear(renderer);
 	SDL_SetRenderDrawColor(renderer,255,255,255,SDL_ALPHA_OPAQUE);
-	mat4x4 matRotZ={0},matRotX={0};
+	mat4x4 matRotZ={0},matRotX={0},matRotY={0};
 	fTheta+=speedOfRotation*deltaTime;
 	// Rotation Z
 	matRotZ[0][0]=cosf(fTheta);
-	matRotZ[0][1]=sinf(fTheta);
-	matRotZ[1][0]=-sinf(fTheta);
+	matRotZ[0][1]=-sinf(fTheta);//sinf
+	matRotZ[1][0]=sinf(fTheta);//-sinf
 	matRotZ[1][1]=cosf(fTheta);
-	matRotZ[2][2]=1;
-	matRotZ[3][3]=1;
+	matRotZ[2][2]=1.0f;
+	matRotZ[3][3]=1.0f;
 	// Rotation X
-	matRotX[0][0]=1;
-	matRotX[1][1]=cosf(fTheta*0.5f);
-	matRotX[1][2]=sinf(fTheta*0.5f);
-	matRotX[2][1]=-sinf(fTheta*0.5f);
-	matRotX[2][2]=cosf(fTheta*0.5f);
-	matRotX[3][3]=1;
+	matRotX[0][0]=1.0f;
+	matRotX[1][1]=cosf(fTheta);
+	matRotX[1][2]=-sinf(fTheta);//sinf
+	matRotX[2][1]=sinf(fTheta);//-sinf
+	matRotX[2][2]=cosf(fTheta);
+	matRotX[3][3]=1.0f;
 
 	std::vector<triangle> vecTrianglesToRaster;
 
 	// Draw Triangles
 	for (const auto& tri:meshObject){
-		triangle triProjected, triTranslated, triRotatedZ, triRotatedZX;
+		triangle triProjected,triTranslated,triRotatedZ,triRotatedZX;
 		// Rotate in Z-Axis
-		MultiplyMatrixVector(tri.p[0], triRotatedZ.p[0], matRotZ);
-		MultiplyMatrixVector(tri.p[1], triRotatedZ.p[1], matRotZ);
-		MultiplyMatrixVector(tri.p[2], triRotatedZ.p[2], matRotZ);
+		MultiplyMatrixVector(tri.p[0],triRotatedZ.p[0],matRotZ);
+		MultiplyMatrixVector(tri.p[1],triRotatedZ.p[1],matRotZ);
+		MultiplyMatrixVector(tri.p[2],triRotatedZ.p[2],matRotZ);
 		// Rotate in X-Axis
-		MultiplyMatrixVector(triRotatedZ.p[0], triRotatedZX.p[0], matRotX);
-		MultiplyMatrixVector(triRotatedZ.p[1], triRotatedZX.p[1], matRotX);
-		MultiplyMatrixVector(triRotatedZ.p[2], triRotatedZX.p[2], matRotX);
-		// Offset into the screen
-		triTranslated = triRotatedZX;
-		triTranslated.p[0].z=triRotatedZX.p[0].z+8.0f;
-		triTranslated.p[1].z=triRotatedZX.p[1].z+8.0f;
-		triTranslated.p[2].z=triRotatedZX.p[2].z+8.0f;
+		MultiplyMatrixVector(triRotatedZ.p[0],triRotatedZX.p[0],matRotX);
+		MultiplyMatrixVector(triRotatedZ.p[1],triRotatedZX.p[1],matRotX);
+		MultiplyMatrixVector(triRotatedZ.p[2],triRotatedZX.p[2],matRotX);
 
-		vec3d normal,line1,line2;
-		line1.x=triTranslated.p[1].x-triTranslated.p[0].x;
-		line1.y=triTranslated.p[1].y-triTranslated.p[0].y;
-		line1.z=triTranslated.p[1].z-triTranslated.p[0].z;
-		
-		line2.x=triTranslated.p[2].x-triTranslated.p[0].x;
-		line2.y=triTranslated.p[2].y-triTranslated.p[0].y;
-		line2.z=triTranslated.p[2].z-triTranslated.p[0].z;
+		triTranslated=triRotatedZX;
 
-		normal.x=line1.y*line2.z-line1.z*line2.y;
-		normal.y=line1.z*line2.x-line1.x*line2.z;
-		normal.z=line1.x*line2.y-line1.y*line2.x;
-		float normalLen=sqrt(normal.x*normal.x+normal.y*normal.y+normal.z*normal.z);
-		normal.x/=normalLen;normal.y/=normalLen;normal.z/=normalLen;
+		triTranslated.p[0].z+=8.0f;
+		triTranslated.p[1].z+=8.0f;
+		triTranslated.p[2].z+=8.0f;
 
-		if(normal.x*(triTranslated.p[0].x-camera.x)+
-		normal.y*(triTranslated.p[0].y-camera.y)+
-		normal.z*(triTranslated.p[0].z-camera.z)<0.0f){
-			MultiplyMatrixVector(triTranslated.p[0], triProjected.p[0], matProj);
-			MultiplyMatrixVector(triTranslated.p[1], triProjected.p[1], matProj);
-			MultiplyMatrixVector(triTranslated.p[2], triProjected.p[2], matProj);
+		vec3d norm,line1,line2;
+		line1=triTranslated.p[1]-triTranslated.p[0];
+		line2=triTranslated.p[2]-triTranslated.p[0]; 
+		norm=vec3d::normal(line1,line2);
+		if(vec3d::dotProduct(norm,triTranslated.p[0]-camera)<0.0f){
+			MultiplyMatrixVector(triTranslated.p[0],triProjected.p[0],matProj);
+			MultiplyMatrixVector(triTranslated.p[1],triProjected.p[1],matProj);
+			MultiplyMatrixVector(triTranslated.p[2],triProjected.p[2],matProj);
 			// Scale into view
 			triProjected.p[0].x+=1.0f;
 			triProjected.p[0].y+=1.0f;
@@ -141,34 +181,23 @@ void Engine3D::Render(const float speedOfRotation)
 			triProjected.p[1].y*=0.5f*(float)ScreenHeight;
 			triProjected.p[2].x*=0.5f*(float)ScreenWidth;
 			triProjected.p[2].y*=0.5f*(float)ScreenHeight;
-
 			vecTrianglesToRaster.push_back(triProjected);
-
-			/*SDL_FPoint points[4]={triProjected.p[0].x,triProjected.p[0].y,triProjected.p[1].x,triProjected.p[1].y, triProjected.p[2].x,triProjected.p[2].y,triProjected.p[0].x,triProjected.p[0].y};
-			SDL_RenderDrawLinesF(renderer,points,4);*/
-			/*SDL_Vertex verts[3];
-			verts[0].position=points[0];verts[0].color=SDL_Color{255,0,0,255};
-			verts[1].position=points[1];verts[1].color=SDL_Color{255,0,0,255};
-			verts[2].position=points[2];verts[2].color=SDL_Color{255,0,0,255};
-			SDL_RenderGeometry(renderer,nullptr,verts,3,nullptr,0);*/
 		}
-		sort(vecTrianglesToRaster.begin(),vecTrianglesToRaster.end(),[](triangle& t1,triangle& t2)
+		sort(vecTrianglesToRaster.begin(),vecTrianglesToRaster.end(),[](const triangle& t1,const triangle& t2)
 		{
-			float z1=(t1.p[0].z+t1.p[1].z+t1.p[2].z)/3.0f;
-			float z2=(t2.p[0].z+t2.p[1].z+t2.p[2].z)/3.0f;
+			float z1=(t1.p[0].z+t1.p[1].z+t1.p[2].z);
+			float z2=(t2.p[0].z+t2.p[1].z+t2.p[2].z);
 			return z1>z2;
 		});
 		for(auto& triProjected:vecTrianglesToRaster){
 			SDL_FPoint points[4]={triProjected.p[0].x,triProjected.p[0].y,triProjected.p[1].x,triProjected.p[1].y, triProjected.p[2].x,triProjected.p[2].y,triProjected.p[0].x,triProjected.p[0].y};
 			SDL_RenderDrawLinesF(renderer,points,4);
 			SDL_Vertex verts[3];
-			verts[0].position=points[0];verts[0].color=SDL_Color{255,255,255,255};
-			verts[1].position=points[1];verts[1].color=SDL_Color{255,255,255,255};
-			verts[2].position=points[2];verts[2].color=SDL_Color{255,255,255,255};
+			verts[0].position=points[0];verts[0].color=SDL_Color{255,0,0,255};
+			verts[1].position=points[1];verts[1].color=SDL_Color{0,255,0,255};
+			verts[2].position=points[2];verts[2].color=SDL_Color{0,0,255,255};
 			SDL_RenderGeometry(renderer,nullptr,verts,3,nullptr,0);
 		}
-
-
 	}SDL_RenderPresent(renderer);
 	auto time2=std::chrono::high_resolution_clock::now();
 	duration=time2-time1;
--- engine.hpp
+++ engine.hpp
@@ -10,12 +10,24 @@
 #include <algorithm>
 
 struct vec3d{
+	friend class Engine3D;
 	public:
-		float x,y,z,w;
 		vec3d():x(0.0f),y(0.0f),z(0.0f),w(1.0f){};
 		vec3d(float xCor, float yCor, float zCor):x(xCor),y(yCor),z(zCor),w(1.0f){};
-	public:
-		float dotProduct(const vec3d& vec1,const vec3d& vec2);
+		static float dotProduct(const vec3d& vec1,const vec3d& vec2);
+		static vec3d crossProduct(const vec3d& vec1,const vec3d& vec2);
+		static vec3d normal(const vec3d& vec1,const vec3d& vec2);
+		float len();
+		vec3d operator+(const vec3d& vec);
+		vec3d operator-(const vec3d& vec);
+		vec3d operator*(float scalar);
+		vec3d operator/(float scalar);
+		void operator+=(const vec3d& vec);
+		void operator-=(const vec3d& vec);
+		void operator*=(float scalar);
+		void operator/=(float scalar);  
+	private:
+		float x,y,z,w;
 };
 struct triangle{
   	vec3d p[3];
@@ -27,7 +39,6 @@ class Engine3D{
 	public:
 		Engine3D(SDL_Window* window, SDL_Renderer* render);
 		void Render(const float speedOfRotation);
-		void Create();
 		bool loadMesh(const std::string& filename);
 	private:
 		SDL_Renderer* renderer;
@@ -37,5 +48,6 @@ class Engine3D{
 		float fTheta;
 		float deltaTime=0.0f;
 		int ScreenWidth,ScreenHeight;
-		void MultiplyMatrixVector(const vec3d& i, vec3d& o,const mat4x4& m);
+		static void MultiplyMatrixVector(const vec3d& i,vec3d& o,const mat4x4& m);
+		void CreateProjMatrix();
 };
